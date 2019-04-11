1d4ca2eda49e58713bc8737c5d6806483c056aaa
Dylan Storts <d-storts@onu.edu>
2019-04-11 04:30:20 -0400

    Added a distance finder class

---
+++ EfficientFootWalker/EfficientFootWalker/DistanceFinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfficientFootWalker
+{
+    class DistanceFinder
+    {
+        private List<List<double>> Distances;
+        private AdjacencyList adjacencyList;
+        private int Size;
+
+        public DistanceFinder(int size)
+        {
+            Distances = new List<List<double>>();
+            adjacencyList = new AdjacencyList(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Distances.Add(new List<double>());
+
+                for (int j = 0; j < size; j++)
+                {
+                    Distances[i].Add(double.MaxValue);
+                }
+            }
+        }
+
+        private void Calc(int source)
+        {
+            //do relaxing from single source
+        }
+
+    }
+}
